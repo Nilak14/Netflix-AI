a7e1895c4fc39b6351f2e357bb002c9868f9999e
Nilak Pathak <nilakpathak14@gmail.com>
2024-06-29 19:39:03 +0545

    update routing system

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,24 @@ import AppLayout from './AppLayout'
 import mainStore from './Redux/mainStore'
 import LoginPage from './pages/LoginPage'
 import BrowsePage from './pages/BrowsePage'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Error from './pages/Error'
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <AppLayout />,
-      errorElement: <Error />,
-      children: [
-        {
-          path: '/',
-          element: <LoginPage />,
-        },
-        {
-          path: '/browse',
-          element: <BrowsePage />,
-        },
-      ],
-    },
-  ])
+const App = () => {
   return (
     <>
       <Provider store={mainStore}>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<LoginPage />} />
+              <Route path="browse" element={<BrowsePage />} />
+              <Route path="*" element={<Error />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </>
   )
 }
-
 export default App
