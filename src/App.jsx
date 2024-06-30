54e739a2006086aa83a4920a603fcb1d54f94c42
Nilak Pathak <nilakpathak14@gmail.com>
2024-06-30 10:14:08 +0545

    iRevert "update routing system"
    This reverts commit a7e1895c4fc39b6351f2e357bb002c9868f9999e.
    revert

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,34 @@ import AppLayout from './AppLayout'
 import mainStore from './Redux/mainStore'
 import LoginPage from './pages/LoginPage'
 import BrowsePage from './pages/BrowsePage'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import Error from './pages/Error'
 
-const App = () => {
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '/',
+          element: <LoginPage />,
+        },
+        {
+          path: '/browse',
+          element: <BrowsePage />,
+        },
+      ],
+    },
+  ])
   return (
     <>
       <Provider store={mainStore}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<AppLayout />}>
-              <Route index element={<LoginPage />} />
-              <Route path="browse" element={<BrowsePage />} />
-              <Route path="*" element={<Error />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </Provider>
     </>
   )
 }
+
 export default App
